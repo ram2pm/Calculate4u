8b265d0a3ba07d9236edc110db902c5e362626da
Ramsey <ramseymostafa@hotmail.com>
2023-03-06 22:41:43 +0000

    DESIGN FINALISED

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 
-import { isFunctionTypeNode } from "typescript";
-
 export default function App() {
   const operations = [
     { value: "+", operation: "add" },
@@ -14,15 +12,15 @@ export default function App() {
     { value: "AC", operation: "clear" },
   ];
   const numbers = [
-    { value: 7, id: "nine" },
+    { value: 7, id: "seven" },
     { value: 8, id: "eight" },
-    { value: 9, id: "seven" },
-    { value: 4, id: "six" },
+    { value: 9, id: "nine" },
+    { value: 4, id: "four" },
     { value: 5, id: "five" },
-    { value: 6, id: "four" },
-    { value: 1, id: "three" },
+    { value: 6, id: "six" },
+    { value: 1, id: "one" },
     { value: 2, id: "two" },
-    { value: 3, id: "one" },
+    { value: 3, id: "three" },
     { value: 0, id: "zero" },
   ];
 
@@ -30,6 +28,7 @@ export default function App() {
   let lastType;
 
   const handleInput = (val, type) => {
+    console.log(type);
     let display = document.getElementById("display");
     let dVal = display.innerText;
 
@@ -146,50 +145,65 @@ export default function App() {
     }
   };
 
-  const SmallBtn = ({ value, type }) => {
+  const buttonStyles = (value) => {
+    const isOperation = operations.find((item) => item.value === value);
+    if (isOperation && value !== "AC") {
+      return "operation";
+    } else if (value === "AC") {
+      return "AC";
+    } else if (value !== 0) {
+      return "button";
+    } else {
+      return "zero";
+    }
+  };
+
+  const SmallBtn = ({ value, type, width }) => {
     return (
       <button
         id={type}
         onClick={(e) => handleInput(value, type, e)}
-        className="col-3 d-flex justify-content-center align-items-center"
-        style={{
-          borderRight: "1px solid grey",
-          borderBottom: "1px solid grey",
-          height: "3em",
-          backgroundColor: "#151515",
-          padding: "0",
-        }}
+        className={buttonStyles(value)}
       >
         <p style={{ margin: "0" }}>{value}</p>
       </button>
     );
   };
 
+  const outputStyles = {
+    borderRadius: "3px 3px 0 0",
+    fontSize: "1.3em",
+    backgroundColor: "black",
+    marginTop: "3em",
+    height: "4em",
+    width: "100%",
+    fontWeight: "bold",
+  };
+
   return (
     <div className="App d-flex justify-content-center align-items-center">
-      <div className="container w-25 h-75 bg-white rounded d-flex flex-column p-0">
+      <div id="calculator">
         <div
           id="output"
-          className="d-flex align-items-center justify-content-end pe-3"
-          style={{
-            fontSize: "1.3em",
-            backgroundColor: "black",
-            height: "2.5em",
-            width: "100%",
-            fontWeight: "bold",
-          }}
+          className="d-flex align-items-center justify-content-end"
+          style={outputStyles}
         >
-          <p id="display" style={{ margin: "0" }}>
-            0
-          </p>
+          <p id="display">0</p>
         </div>
 
-        <div className="d-flex flex-column">
-          <div
-            className="row p-0 ms-3 d-flex w-75 flex-start"
-            style={{ marginTop: "1em", width: "95%" }}
-          >
-            {numbers.map((item) => {
+        <div id="keys-container">
+          <SmallBtn width="AC" value="AC" type="clear" key={Math.random()} />
+          <SmallBtn value="/" type="divide" key={Math.random()} />
+
+          {numbers.slice(0, 3).map((item) => {
+            return (
+              <SmallBtn value={item.value} type={item.id} key={Math.random()} />
+            );
+          })}
+
+          <SmallBtn value="*" type="multiply" key={Math.random()} />
+          {numbers.slice(3, 6).map((item) => {
+            if (item.value !== 0) {
               return (
                 <SmallBtn
                   value={item.value}
@@ -197,21 +211,58 @@ export default function App() {
                   key={Math.random()}
                 />
               );
-            })}
+            }
+          })}
 
-            <SmallBtn value="." type="decimal" />
+          <SmallBtn value="-" type="subtract" key={Math.random()} />
 
-            {/* {operations.map((item) => {
-              return (
-                <SmallBtn
-                  value={item.value}
-                  type={item.operation}
-                  key={Math.random()}
-                />
-              );
-            })} */}
-          </div>
+          {numbers.slice(6, 9).map((item) => {
+            return (
+              <SmallBtn value={item.value} type={item.id} key={Math.random()} />
+            );
+          })}
+          <SmallBtn value="+" type="add" key={Math.random()} />
+
+          <SmallBtn width="zero" value={0} type="zero" key={Math.random()} />
+          <SmallBtn value="." type="decimal" key={Math.random()} />
+          <SmallBtn value="=" type="equals" key={Math.random()} />
         </div>
+
+        {/* {operations.map((item) => {
+          if (item.value === "AC")
+          return (
+              <SmallBtn
+                width="wide"
+                value={item.value}
+                type={item.id}
+                key={Math.random()}
+              />
+            );
+        })}
+        {numbers.map((item) => {
+          return (
+            <SmallBtn
+              width="small"
+              value={item.value}
+              type={item.id}
+              key={Math.random()}
+            />
+          );
+        })}
+
+        {operations.map((item) => {
+          if (item.value !== "AC")
+            return (
+              <SmallBtn
+                width="small"
+                value={item.value}
+                type={item.id}
+                key={Math.random()}
+              />
+            );
+        })}
+
+        <SmallBtn width="small" value="." type="decimal" /> */}
       </div>
     </div>
   );
